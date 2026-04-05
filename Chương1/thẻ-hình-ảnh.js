695a19d1d4ee6c588a71e14057ffdc492b027134
khoandptit-svg <khoand.ptit@gmail.com>
2026-04-05 22:36:32 +0700

    commit1

---
+++ Chương1/thẻ-hình-ảnh.js
@@ -0,0 +1,15 @@
+/* Thẻ hình ảnh <img>
++ src : thuộc tính bắt buộc , chứa đường dẫn đến hình ảnh
++ width : thuộc tính tùy chọn , xác định chiều rộng của hình ảnh (px hoặc %)
++ height : thuộc tính tùy chọn , xác định chiều cao của hình ảnh (px hoặc %)
++ alt : thuộc tính tùy chọn , cung cấp văn bản thay thế khi hình ảnh không hiển thị
++ border: thuộc tính tùy chọn , xác định độ dày đường viền xung quanh hình ảnh (px)
+
+
+
+
+
+
+
+
+*/
---
+++ Chương1/thẻ-định-dạng-cơ-bản.js
@@ -0,0 +1,57 @@
+/* Các thẻ định dạng văn bản
++ Nhóm thẻ tiêu đề : <h1> đến <h6> và thẻ văn bản <p>
+
++ Nhóm thẻ nhấn mạnh : <b>, <i>, <u>, <strong>, <em>
+
++ Nhóm ngắt dóng và phân tách : <br>, <hr>
+
++ Văn bản dạng nguyên bản : <pre> giữ nguyên khoảng trắng và xuống dòng
+
+MỤC TIÊU : Tạo ra một trang giới thiệu ngắn: 
++ Tiêu đề 
++ Đoạn văn 
++ Điểm nhấn
++ Dễ đọc , dễ dàng 
+
+NHÓM THẺ TIÊU ĐỀ 
++ <h1> là tiêu đề lớn nhất , thường dùng cho chủ đề chính của trang
++ <h6> là thẻ tiêu đề nhỏ nhất 
++ <h2>...<h3> tạo mục và tiểu mục , giúp định hướng nội dung
+
+NHÓM THẺ NHẤN MẠNH
++ <b> in đậm - mang tính trình bày 
++ <i> in nghiêng - thường dụng cho thuật nhữ hoặc trích dẫn
++ <u> gạch chân - dùng nhấn mạnh hoặc ghi chú
++ <strong> tuong tự <b> nhưng mang ý nghĩa "quan trọng" (tốt cho SEO)
++ <em> tương tự <i> nhưng mang ý nghĩa "nhấn mạnh" cảm xúc
+
+NHÓM NGẮT DÒNG VÀ PHÂN TÁCH
++ <br> dùng để xuống dòng ngay tại vị trí chè thẻ
+-Lưu ý trong html , enter xuống dòng trg code làm nd xuống dòng , trừ khi dung <br>
+
++ <hr> tạo 1 đg kẻ ngang để phân tách các phần nd
+có thuộc tính : size (độ dày), width (độ dài), color (màu sắc), align (căn lề : left, center, right)
+
+THẺ VĂN BẢN DẠNG NGUYÊN BẢN
++ <pre> giữ nguyên khoảng trắng các ký tự xuống dòng
+
+
+THẺ LIÊN KẾT VÀ THẺ DANH SÁCH 
++ <a> tạo liên kết đến trang khác hoặc phần khác của cùng trang
++ <ul> tạo danh sách không thứ tự (dùng với <li>)
++ <ol> tạo danh sách có thứ tự (dùng với <li>)
++ <li> tạo mục trong danh sách
+
+viết tắt : ul>li*3 , ol>li*3 
+
+
+
+
+
+
+
+
+
+
+
+*/
